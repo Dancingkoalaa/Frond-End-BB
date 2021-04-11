b68f5a48ad03462e586e697e4767936bd4e9fdac
Dancingkoalaa <75275707+Dancingkoalaa@users.noreply.github.com>
2021-04-11 20:44:20 +0200

    Add files via upload

---
+++ UseGuide.php
@@ -0,0 +1,35 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>UserGuide</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/Home.css">
+    <script src="js/Home.js"></script>
+</head>
+<body>
+
+<nav class="topnav" id="myTopnav">
+    <a href="Home.php">Home</a>
+    <a href="UseGuide.php" class="active">User Guide</a>
+    <a href="Contact.php">Contact</a>
+    <a id='flip-menu' class="icon">
+        <i class="fa fa-bars"></i>
+    </a>
+</nav>
+
+<main>
+    <h2>Spotify Music website!!</h2>
+    <section>
+        We don’t serve their kind here! What? Your droids. They’ll have to wait outside. We don’t want them here.
+        Listen, why don’t you wait out by the speeder. We don’t want any trouble. I heartily agree with you sir.
+        Negola dewaghi wooldugger?!? He doesn’t like you. I’m sorry. I don’t like you either You just watch yourself.
+        We’re wanted men. I have the death sentence in twelve systems. I’ll be careful than. You’ll be dead.
+        This little one isn’t worth the effort. Come let me buy you something…
+    </section>
+</main>
+
+</body>
+</html>
